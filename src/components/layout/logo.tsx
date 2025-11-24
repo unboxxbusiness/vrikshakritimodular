6f37bd4a4561b24310b5ab8e2c30a55a50a8d8d3
Anuj Sharma <anujsharmatalks@gmail.com>
2025-11-24 08:15:56 +0000

    use this icon as logo and make sure its hould be visible across devices

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image
-      src="https://res.cloudinary.com/dhrigocvd/image/upload/v1763971951/logo_for_website_jv4hg6.png"
+      src="https://res.cloudinary.com/dhrigocvd/image/upload/v1763972114/Gemini_Generated_Image_rw369trw369trw36-removebg-preview_1_utsfjk.png"
       alt="Vrikshakriti Logo"
-      width={40}
+      width={120}
       height={40}
-      className="h-10 w-10"
+      className="h-10 w-auto"
     />
   );
 }
